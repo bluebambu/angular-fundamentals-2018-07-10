4da435b53d37c87a50198b95e9398e1d534d10a6
Peter Munro <peter@petermunro.org>
2018-07-11 08:53:50 -0400

    Further components & routing developed during class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tuesday\'s Angular class';
+  i = 3 + this.title;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { FooComponent } from './hello.component';
+import { AlertButtonComponent } from './alert.component';
+import { CounterComponent } from './counter.component';
+import { MouseTrackerComponent } from './mouse-tracker.component';
+import { ImageSelectorComponent } from './image-selector.component';
+import { NgForDemoComponent } from './ngfor-demo.component';
+import { InputExampleComponent } from './input-example.component';
+
+import routes from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooComponent,
+    AlertButtonComponent,
+    MouseTrackerComponent,
+    ImageSelectorComponent,
+    NgForDemoComponent,
+    InputExampleComponent,
+    CounterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
